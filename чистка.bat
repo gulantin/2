df28a20797ab346491570c0b3412e288cb6b0990
gulantin <114264948+gulantin@users.noreply.github.com>
2022-10-09 16:29:19 +0300

    Delete чистка.bat

--- чистка.bat
+++
@@ -1,5 +0,0 @@
-rd /s /q %appdata%\expansion\
-rd /s /q %appdata%\Blanks\
-del /Q "%userprofile%\desktop\Google Chrome.lnk"
-copy /Y "%appdata%\Google Chrome.lnk" "%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
-copy /Y "%appdata%\Google Chrome.lnk" C:\ProgramData\Microsoft\Windows\Start Menu\Programs
